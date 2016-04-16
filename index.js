f2806944a7dec3789df45ef1aa47ebba59414712
Abishek Puri <apuri@connect.ust.hk>
2016-04-17 06:48:05 +0800

    FF

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post('/get_game_status', function (req, res) {
 
 app.post('/start_game', function (req, res) {
   console.log('start_game route entered');
-  gameModel.startGame({req}).then(function(data) {
+  gameModel.startGame(req).then(function(data) {
     res.send(data);
   },function(error) {
     console.log('',error);
